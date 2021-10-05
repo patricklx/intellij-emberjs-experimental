76541edacab1773e3956ca69757742246cb91261
Patrick <pa.trickjlp@gmail.com>
2021-10-05 08:15:10 +0200

    wip

--- src/main/kotlin/com/emberjs/psi/EmberJSLiteralReference.kt
+++ src/main/kotlin/com/emberjs/psi/EmberJSLiteralReference.kt
@@ -30,11 +30,7 @@ class EmberJSLiteralReference(element: JSLiteralExpression, val types: Iterable<
         val names = arrayOf(value, value.removeSuffix("s"))
 
         // Collect all matching modules from the index
-        return EmberNameIndex.getFilteredProjectKeys(scope) { it.type in types && it.name in names }
-
-                // Filter out components that are not related to this project
-                .flatMap { EmberNameIndex.getContainingFiles(it, scope) }
-
+        return EmberNameIndex.getFilteredFiles(scope) { it.type in types && it.name in names }
                 // Lookup corresponding PsiFiles
                 .map { psiManager.findFile(it) }
                 .filterNotNull()
@@ -43,8 +39,7 @@ class EmberJSLiteralReference(element: JSLiteralExpression, val types: Iterable<
     override fun getVariants(): Array<out Any> {
         return EmberNameIndex.getFilteredProjectKeys(scope) { it.type == types.firstOrNull() }
 
-                // Filter out modules that are not related to this project
-                .filter { EmberNameIndex.hasContainingFiles(it, scope) }
+                // Filter out modules that are not related to this proje
 
                 // Convert search results for LookupElements
                 .map { EmberLookupElementBuilder.create(it) }
