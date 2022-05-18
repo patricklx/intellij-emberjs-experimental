cac040c45fd1592001e06470aa0ea3fe7b44bbe6
patricklx <patricklx@users.noreply.github.com>
2022-05-18 16:21:51 +0200

    fix another assert

--- src/main/kotlin/com/emberjs/psi/EmberInjectionAnnotator.kt
+++ src/main/kotlin/com/emberjs/psi/EmberInjectionAnnotator.kt
@@ -42,8 +42,8 @@ class EmberInjectionAnnotator : Annotator {
         // Create an annotation with a gutter icon renderer
         holder.createInfoAnnotation(identifier, null).apply {
             val navHandler = DefaultGutterIconNavigationHandler<PsiElement>(referencedFiles, name.displayName)
-            val lmi = LineMarkerInfo(identifier, identifier.textRange, icon, Pass.LINE_MARKERS,
-                    null, navHandler, GutterIconRenderer.Alignment.CENTER)
+            val lmi = LineMarkerInfo(identifier, identifier.textRange, icon,
+                    null, navHandler, GutterIconRenderer.Alignment.CENTER, { "go to related" })
 
             gutterIconRenderer = LineMarkerInfo.LineMarkerGutterIconRenderer(lmi)
         }
