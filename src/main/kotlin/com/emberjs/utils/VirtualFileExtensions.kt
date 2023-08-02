b06820eb9e4b0a2c2f3cc84b67a6f2c8f7b374e1
Patrick Pircher <patricklx@users.noreply.github.com>
2023-08-02 11:52:57 +0200

    improve in repo addons resolutions (#47)

--- src/main/kotlin/com/emberjs/utils/VirtualFileExtensions.kt
+++ src/main/kotlin/com/emberjs/utils/VirtualFileExtensions.kt
@@ -1,13 +1,11 @@
 package com.emberjs.utils
 
+import com.dmarcotte.handlebars.parsing.HbParseDefinition
 import com.google.gson.stream.JsonReader
 import com.intellij.javascript.nodejs.PackageJsonData
 import com.intellij.openapi.vfs.VirtualFile
-import com.intellij.util.text.CharSequenceReader
-import java.io.IOException
-import com.dmarcotte.handlebars.parsing.HbParseDefinition
 import com.intellij.psi.PsiElement
-import org.jetbrains.annotations.Nullable
+import com.intellij.util.text.CharSequenceReader
 import org.mozilla.javascript.ast.AstNode
 
 val VirtualFile.parents: Iterable<VirtualFile>
@@ -27,7 +25,45 @@ val VirtualFile.parents: Iterable<VirtualFile>
 
 
 val addonCache = HashMap<String, Boolean>()
+val inRepoCache = HashMap<String, List<String>>()
 val emberCache = HashMap<String, Boolean>()
+
+val VirtualFile.inRepoAddonPaths: List<String>
+    get() {
+        val out = mutableListOf<String>()
+        if (inRepoCache.contains(this.path)) return inRepoCache.getOrDefault(this.path, emptyList())
+        val packageJsonFile = findFileByRelativePath("package.json") ?: return out
+        val text: String
+        try {
+            text = String(packageJsonFile.contentsToByteArray())
+            val reader = JsonReader(CharSequenceReader(text))
+            reader.isLenient = true
+            reader.beginObject()
+            while (reader.hasNext()) {
+                val key = reader.nextName()
+                if (key == "ember-addon") {
+                    reader.beginObject()
+                    while (reader.hasNext()) {
+                        val k = reader.nextName()
+                        if (k == "paths") {
+                            reader.beginArray()
+                            while (reader.hasNext()) {
+                                out.add(reader.nextString())
+                            }
+                            reader.endArray()
+                        }
+                        reader.skipValue()
+                    }
+                    reader.endObject()
+                }
+                reader.skipValue()
+            }
+            inRepoCache[this.path] = out
+            return out
+        } catch (var3: Exception) {
+            return out
+        }
+    }
 
 val VirtualFile.isEmberAddonFolder: Boolean
     get() {
@@ -41,10 +77,6 @@ val VirtualFile.isEmberAddonFolder: Boolean
             reader.beginObject()
             while (reader.hasNext()) {
                 val key = reader.nextName()
-                if (key == "ember-addon") {
-                    addonCache[this.path] = true
-                    return true
-                }
                 if (key == "keywords") {
                     reader.beginArray()
                     while (reader.hasNext()) {
@@ -115,10 +147,25 @@ val VirtualFile.isEmberFolder: Boolean
         }
     }
 
+
+fun getByPath(directory: VirtualFile, path: String): VirtualFile? {
+    val parts = path.split("/")
+    var current = directory as VirtualFile?
+    parts.forEach { p ->
+        current = current?.children?.find { it.name == p }
+    }
+    return current
+}
+
+val VirtualFile.inRepoAddonDirs: List<VirtualFile>
+    get() = this.isEmber.ifFalse {
+       emptyList()
+    } ?: inRepoAddonPaths.mapNotNull { getByPath(this, it) }
+
 val VirtualFile.isInRepoAddon: Boolean
     get() = findFileByRelativePath("package.json") != null &&
-            parent?.name == "lib" &&
-            parent?.parent?.isEmberFolder == true
+            parentEmberModule?.inRepoAddonDirs?.any { it == this } == true
+
 
 /**
  * Searches all parent paths until it finds a path containing a `package.json` file.
