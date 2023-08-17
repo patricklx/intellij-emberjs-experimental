5de83b6bcb5e2b306055680fde36d62483368d32
Patrick Pircher <patricklx@users.noreply.github.com>
2023-08-17 13:24:22 +0200

    provide live templates for gjs/gts (#67)

--- src/main/kotlin/com/emberjs/template/EmberContext.kt
+++ src/main/kotlin/com/emberjs/template/EmberContext.kt
@@ -1,13 +1,14 @@
 package com.emberjs.template
 
-import com.dmarcotte.handlebars.file.HbFileType
+import com.dmarcotte.handlebars.file.HbFileViewProvider
+import com.emberjs.gts.GtsFileViewProvider
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
 
-class EmberContext : TemplateContextType("EMBER", "Ember") {
+class EmberContext : TemplateContextType("Ember") {
 
     override fun isInContext(context: TemplateActionContext): Boolean {
-        return context.getFile().fileType is HbFileType
+        return context.file.viewProvider is HbFileViewProvider || context.file.viewProvider is GtsFileViewProvider
     }
 }
